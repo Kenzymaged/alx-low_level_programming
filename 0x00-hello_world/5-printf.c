6ac8e4b8c874cf4e70a9bfd416a9d1235b29f553
kenzymaged <kenzymaged2004@gmail.com>
2022-09-08 18:43:18 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print with printf function
+ *
+ * description: using the main function
+ * program prints the statement with proper grammar
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
