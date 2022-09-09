84a91ba7f0d531e70ee6e80050267e791b03d0f2
kenzymaged <kenzymaged2004@gmail.com>
2022-09-08 19:46:47 -0700

    aaaaaaaaaaaaa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,21 +5,14 @@
  *
  * description: using the main function
  * the program prints the statement
- * Return: 0
+ * Return: 0 if exited properly,non-zero otherwise
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	fprintf(stderr, "error");
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
