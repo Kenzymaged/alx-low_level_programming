7921d8f58b75a4d221ea3459e6392abb78288397
kenzymaged <kenzymaged2004@gmail.com>
2022-09-08 19:29:37 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,5 +20,6 @@ int main(void)
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
+	fprintf("error")
 	return (0);
 }
